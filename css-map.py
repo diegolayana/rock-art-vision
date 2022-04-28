e20f8351113fea9408ba884334d9cceaaaf3f703
Diego Layana <diegolayanaflores@gmail.com>
2022-04-28 16:33:19 -0400

    Modified css-map.py A lot of changes were made in css-map.py file. It correctly compute que contour scale space map using a custom convolution for closed curves from utils.py

--- css-map.py
+++ css-map.py
@@ -1,42 +1,29 @@
-from random import gauss
 from boundary_pts import *
 from imagen import *
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 from scipy.ndimage import gaussian_filter1d
+from scipy.signal import convolve
 import numpy as np
+from utils import *
 
-
-def ku_compute(img, n_pts, sigma):
+def compute_ku(img, n_pts, sigma, max_sigma):
 
     x1,y1,pts = boundary_pts(img, n_pts)
-    
-    N = 1000
-    dx = float(len(x1))/N
 
-    x = np.arange(-3*sigma, 3*sigma, dx)
-    gaussian = np.exp(-(x/sigma)**2/2)
-    gaussian_1d = -(x/sigma**2)*np.exp(-(x/sigma)**2/2)
-    gaussian_2d = ((x**2/sigma**4)-(1/sigma**2))*np.exp(-(x/sigma)**2/2)
-    
-    x1 = x1/np.amax(x1)
-    y1 = y1/np.amax(y1)
+    x = np.linspace(-4*max_sigma, 4*max_sigma, n_pts)
+    g, g1, g2 = compute_gaussian(x, sigma)
 
-    xg = np.convolve(x1, gaussian,0)
-    yg = np.convolve(y1, gaussian,0)
+    xg = gaussian_conv(x1, sigma, max_sigma)
+    yg = gaussian_conv(y1, sigma, max_sigma)
 
-    xg = xg/np.amax(xg)
-    yg = yg/np.amax(yg)
+    xu = gaussian1d_conv(x1, sigma, max_sigma)
+    yu = gaussian1d_conv(y1, sigma, max_sigma) 
 
-    xu = np.convolve(x1, gaussian_1d)
-    yu = np.convolve(y1, gaussian_1d) 
-
-    xuu = np.convolve(x1, gaussian_2d)
-    yuu = np.convolve(y1, gaussian_2d) 
-
+    xuu = gaussian2d_conv(x1, sigma, max_sigma)
+    yuu = gaussian2d_conv(y1, sigma, max_sigma)
 
     ku = (xu*yuu - xuu*yu)/(xu**2+yu**2)**(3/2)
-    ku = np.interp(np.arange(0, n_pts),np.arange(0, len(ku)), ku)
     asign = np.sign(ku)
     signchange = ((np.roll(asign, 1) - asign) != 0).astype(int)
 
@@ -48,72 +35,25 @@ def plot_ku(img, n_pts, max_sigma):
 
     css_map = np.zeros(shape=(1,n_pts))
     for sigma in std:
-        newrow = ku_compute(img, n_pts, sigma)
+        newrow = compute_ku(img, n_pts, sigma, max_sigma)
         css_map = np.vstack([css_map, newrow])
-
+    css_map = css_map[0:n_pts][0:]
+    print(css_map.shape)
     return css_map
 
-def gaussian_slider():
-
-    dim = 200
-    scale = 0.1
-
-    img_camelido = Imagen(r'C:\Users\diego\Desktop\Programacion\rock-art-vision\images\images_raw_all\Ll-43_B5-I_F6.tif')
-    img_camelido.start(scale, dim)
-    x1,y1,pts = boundary_pts(img_camelido.img, 400)
-
-    fig, ax = plt.subplots()
-    line, = plt.plot(y1, -x1)
-
-    plt.subplots_adjust(left=0.25, bottom=0.15)
-
-
-    axstd = plt.axes([0.1, 0.25, 0.0225, 0.63])
-    std_slider = Slider(
-        ax=axstd,
-        label="Desviacion estandar",
-        valmin=0,
-        valmax=50,
-        valinit=0,
-        orientation="vertical"
-    )
-
-
-    # The function to be called anytime a slider's value changes
-    def update(val):
-        x1,y1,pts = boundary_pts(img_camelido.img, 400)
-        x2 = gaussian_filter1d(x1,std_slider.val)
-        y2 = gaussian_filter1d(y1,std_slider.val)
-        line.set_data(y2, -x2)
-        fig.canvas.draw_idle()
-
-
-    # register the update function with each slider
-    std_slider.on_changed(update)
-
-    # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-    resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-    button = Button(resetax, 'Reset', hovercolor='0.975')
-
-
-    def reset(event):
-        std_slider.reset()
-    button.on_clicked(reset)
-
-    plt.show()
-
 def main():
-    dim = 200
+    dim = 400
     scale = 0.1
     n_pts = 400
 
-    img_camelido = Imagen(r'C:\Users\diego\Desktop\Programacion\rock-art-vision\images\images_raw_all\Ll-43_B5-I_F6.tif')
+    img_camelido = Imagen(r'C:\Users\diego\Desktop\Programacion\rock-art-vision\images\images_raw_all\Antropomorfos\Ll-43_B52_P1-F4.tif')
     img_camelido.start(scale, dim)
-    
-    x, y, pts = boundary_pts(img_camelido.img, n_pts) 
-    plt.plot(y,-x)
+
+    css_map = plot_ku(img_camelido.img, n_pts, 6)
+    plt.imshow(img_camelido.img, 'gray')
     plt.show()
-
-
+    plt.imshow(css_map, 'gray')
+    plt.show()
+    
 if __name__ == '__main__':
     main()
