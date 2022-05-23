7b7d7277ddb424c44e8a72ad06571658eb35aae4
Diego Layana <diegolayanaflores@gmail.com>
2022-05-23 17:54:37 -0400

    Modified utils.py Gaussian funtions were wrong, they were fixed with Mauro Bologna help

--- utils.py
+++ utils.py
@@ -1,15 +1,10 @@
-from boundary_pts import *
-from imagen import *
 import numpy as np
 import matplotlib.pyplot as plt
-from boundary_pts import *
-from imagen import *
 
 def compute_elipse(theta, h, k, a, b):
     x = a*np.cos(theta) + h
     y = b*np.sin(theta) + h
     return x, y
-
 
 def compute_circle(theta, h, k, r):
     x = r*np.cos(theta) + h
@@ -17,14 +12,23 @@ def compute_circle(theta, h, k, r):
     return x, y
 
 def compute_gaussian(x, sigma):
-    gaussian = np.exp(-(x/sigma)**2/2)
-    gaussian_1d = -(x/sigma**2)*np.exp(-(x/sigma)**2/2)
-    gaussian_2d = ((x**2/sigma**4)-(1/sigma**2))*np.exp(-(x/sigma)**2/2)
+    g = gaussian(x, sigma)
+    g1 = gaussian1d(x, sigma)
+    g2 = gaussian2d(x, sigma)
+    return g , g1, g2
 
-    return gaussian, gaussian_1d, gaussian_2d
+def gaussian(x, sigma):
+    g = np.exp(-(x**2/(2*(sigma)**2)))/(sigma*np.sqrt(2*np.pi))
+    return g
 
+def gaussian1d(x,sigma):
+    g = -(np.exp(-(x**2)/(2*sigma**2))*x)/(np.sqrt(2*np.pi)*sigma**3)
+    return g
 
-#We're gonna compute convolve function for close cruves.
+def gaussian2d(x,sigma):
+    g = (np.exp(-(x**2)/(2*sigma**2))*(-sigma**2 + x**2))/(np.sqrt(2*np.pi)*sigma**5)
+    return g
+
 def gaussian_conv(arr, sigma, max_sigma):
     n_pts = len(arr)
     t = np.linspace(-3*max_sigma, 3*max_sigma, n_pts)
@@ -56,26 +60,7 @@ def gaussian2d_conv(arr, sigma, max_sigma):
     return np.array(conv)
 
 def main():
-    sigma = 6
-    max_sigma = 50
-    dim = 200
-    scale = 0.1
-    n_pts = 400
-
-    img_camelido = Imagen(r'C:\Users\diego\Desktop\Programacion\rock-art-vision\images\images_raw_all\Ll-43_B5-I_F6.tif')
-    img_camelido.start(scale, dim)
-
-    x1,y1,pts = boundary_pts(img_camelido.img, n_pts)
-
-    convx = gaussian_conv(x1, sigma, max_sigma)
-    convy = gaussian_conv(y1, sigma, max_sigma)
-    convx = convx/np.amax(convx)
-    convy = convy/np.amax(convy)
-
-    x1 = x1/np.amax(x1)
-    y1 = y1/np.amax(y1)
-    plt.plot(convy,-convx)
-    plt.show()
+    pass
 
 if __name__ == '__main__':
     main()
