0fcd3ad0806c5b0044f94ff6aac1617430f8a0d4
diegolayana <diegolayanaflores@gmail.com>
2022-07-14 19:08:38 -0400

    Updated create_db.py The problems were it failed were fixed

--- create_db.py
+++ create_db.py
@@ -6,15 +6,15 @@ import numpy as np
 #The porpuse os this code is to create the database where the matching algorithm will work
 
 def main():
-    scale = 0.15
+    max_size = 400
     n_pts = 400
     sigma = 1
     dirname = os.path.dirname(__file__)
-    images_path = join(dirname, r'images\images_test')
-    css_map_path = join(dirname, r'images\css-maping')
+    images_path = join(dirname, r'images\db_test')
+    css_map_path = join(dirname, r'images\css_map_test')
     images_list = [f for f in listdir(images_path) if isfile(join(images_path,f))]
     for image in images_list:
-        img = Imagen(join(images_path,image), scale)
+        img = Imagen(join(images_path,image), max_size)
         img.plot_ku(n_pts, sigma)
 
         np.save(join(css_map_path,image),img.css_map)
